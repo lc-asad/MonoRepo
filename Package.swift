3dae915ed0fec9e57b1285fde11b7dc76d734c49
Asad Ullah <asad_622@hotmail.com>
2024-01-30 02:16:15 +0500

    changes

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 // date 30-01-2024
+// changes made into PackageB
 import PackageDescription
 
 let package = Package(
