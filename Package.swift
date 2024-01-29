dd94907809c7dd9d765d7d05c226603accf42f2a
Asad Ullah <asad_622@hotmail.com>
2024-01-30 01:19:20 +0500

    - changes

--- Package.swift
+++ Package.swift
@@ -7,17 +7,20 @@ let package = Package(
     name: "Monorepo",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Monorepo",
-            targets: ["Monorepo"]),
+        .library(name: "Monorepo", targets: ["Monorepo"]),
+        //.library(name: "PackageA", targets: ["PackageA"]),
+        //.library(name: "PackageB", targets: ["PackageB"]),
+    ],
+    dependencies: [
+        .package(path: "PackageA"),
+        .package(path: "PackageB")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Monorepo"),
-        .testTarget(
-            name: "MonorepoTests",
-            dependencies: ["Monorepo"]),
+        .target(name: "Monorepo"),
+        .target(name: "PackageA", dependencies: [], path: "PackageA/Sources/"),
+        .target(name: "PackageB", dependencies: [], path: "PackageB/Sources/"),
+        .testTarget(name: "MonorepoTests", dependencies: ["Monorepo"]),
     ]
 )
